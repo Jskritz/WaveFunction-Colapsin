e23c1d7f459e2c762da65f7ea2405cfaa3fa1324
Joshua Kritz <joshuakritz2@gmail.com>
2024-02-22 11:19:36 +0000

    add random walk code

--- WaveFunctionColapse/Assets/Scripts/GameManager.cs
+++ WaveFunctionColapse/Assets/Scripts/GameManager.cs
@@ -1,15 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<List<GameObject>> walkGrid = new List<List<GameObject>>();
+
+    public Collapser collapser;
+
+    private RandomWalker walker;
+
+    public GameObject slot;
     void Awake(){
         
     }
     // Start is called before the first frame update
     void Start()
     {
+        //GenerateGrid(50);
+        StartCoroutine(StartWalk());
+    }
+
+    IEnumerator StartWalk(){
+        yield return new WaitForSeconds(1.0f);
+        walker = GetComponent<RandomWalker>();
+        walkGrid = collapser.getGrid();
+        Debug.Log("Size of grid :"+walkGrid.Count);
+        walker.setGizmoGrid(walkGrid);
+        Debug.Log("Starting Walk");
+        walker.RandomWalk();
         
     }
 
@@ -18,4 +38,15 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    void GenerateGrid(int gridSize){
+        for(int i =0 ; i<gridSize;i++){
+            List<GameObject> line = new List<GameObject>();
+            for(int j =0 ; j<gridSize;j++){
+                GameObject cube = Instantiate(slot, new Vector3(i,0,j), Quaternion.identity);
+                line.Add(cube);
+            }
+            walkGrid.Add(line);
+        }
+    }   
 }
